decf9c48487120894d9139472efd88ef0ff93225
Franklin Shera <fshera96@gmail.com>
2022-09-11 20:29:54 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,80 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                paypal: {
+                    1: "#3b7bbf",
+                },
+                facebook: {
+                    1: "#4267B2",
+                },
+                google: {
+                    1: "#DB4437",
+                },
+                github: {
+                    1: "#333333",
+                    2: "#fafafa",
+                    3: "#f5f5f5",
+                },
+                aswhite: {
+                    1: "#ffffffef",
+                    2: "#cccccc55",
+                },
+                primary: {
+                    1: "#F27323",
+                    2: "#FAA63B",
+                    3: "#6989ff",
+                    4: "#00b985",
+                },
+                dark: {
+                    1: "#111111",
+                    2: "#707070",
+                    3: "#727272",
+                    4: "#2B2B2B",
+                    5: "#141414",
+                    6: "#BCBCBC",
+                },
+                input: "#f2f2f2",
+                palbrown: "#54433a",
+                palbrowner: "#402e32",
+                palblue: "#5f89ff",
+                palbluelight: "#6989ff22",
+                palhighlight: "#ebf6f7",
+                asgreen: "#1DD400",
+            },
+            spacing: {
+                "5/100": "5%",
+                "10/100": "10%",
+                "15/100": "15%",
+                "20/100": "20%",
+                "22/100": "22%",
+                "30/100": "30%",
+                "35/100": "35%",
+                "45/100": "45%",
+                "55/100": "55%",
+                "65/100": "65%",
+                "75/100": "75%",
+                "85/100": "85%",
+                "95/100": "95%",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
